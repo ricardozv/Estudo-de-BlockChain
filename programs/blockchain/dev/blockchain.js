33f9b9c096282be6296f53848a041f2ef3c273f2
Ricardo <ricardosilvanet03@gmail.com>
2020-01-09 22:31:29 -0300

    subindo o scrypt da blockchain

---
+++ programs/blockchain/dev/blockchain.js
@@ -0,0 +1,40 @@
+const sha256 = require('sha256');
+
+function Blockchain () {
+    this.chain = [];
+    this.newTransactions = [];
+}
+
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+    const newBlock = {
+        index: this.chain.length + 1,
+        timestamp: Date.now(),
+        transactions : this.newTransactions,
+        nonce: nonce,
+        hash: hash,
+        previousBlockHash: previousBlockHash,
+    };
+    this.newTransactions = [];
+    this.chain.push(newBlock);
+    return newBlock;
+}
+
+Blockchain.prototype.getLastBlock = function () {
+    return this.chain[this.chain.length - 1];
+}
+
+Blockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
+    const newTransaction = {
+        amount: amount,
+        sender: sender,
+        recipient: recipient,
+    };
+
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()['index'] + 1;
+
+};
+
+
+module.exports = Blockchain;
