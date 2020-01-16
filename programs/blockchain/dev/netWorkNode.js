efaf01735bfd97a772d944fc7e331e6392f65e64
ricozv <ricardosilvanet03@gmail.com>
2020-01-16 02:50:32 -0300

    add networknode

---
+++ programs/blockchain/dev/netWorkNode.js
@@ -0,0 +1,12 @@
+app.post('/register-and-broadcast-node', function(req, res){
+    const newNodeUrl = req.body.newNodeUrl;
+
+});
+if(bitcoin.netWorkNodes.indexOf(newNodeUrl) == -1)
+bitcoin.netWorkNodes.push(newNodeUrl);
+
+bitcoin.netWorkNodes.forEach(netWorkNodes => {
+//...
+'/register-node'
+
+});
